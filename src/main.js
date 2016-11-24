387de456c6476528f6f83bb960e5fff96aefd76d
codetector <codetector@codetector.cn>
2016-11-24 16:09:04 -0500

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
+import routes from './routes'
 /* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   el: '#app',
   template: '<App/>',
