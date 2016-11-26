2712012bf02c195da39c3c4b8dbf5478f71ded74
Codetector <codetector@codetector.cn>
2016-11-25 20:12:22 -0500

    ESLint styling change

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     // add your custom rules here
     'rules': {
         // allow paren-less arrow functions
-        'indent': ['warn', 4],
+        'indent': ['warn', 4, {"SwitchCase": 1}],
         'arrow-parens': 0,
         // allow async-await
         'generator-star-spacing': 0,
